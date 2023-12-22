9e3f8e6b62b67edf556d4886fb1b70ca772cc2b5
Elwyn Van der Borght <elwyn.vdb@gmail.com>
2023-12-22 09:17:42 +0100

    feat(Admin BE): Allow bulk controllers to accept array of message identifiers

--- src/Presentation/Controller/Admin/RequeueController.php
+++ src/Presentation/Controller/Admin/RequeueController.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Tailr\SuluMessengerFailedQueueBundle\Presentation\Controller\Admin;
 
+use function Psl\Type\int;
+use function Psl\Type\shape;
+use function Psl\Type\vec;
+
 use Sulu\Component\Security\SecuredControllerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,7 +15,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Tailr\SuluMessengerFailedQueueBundle\Domain\Command\RequeueHandler;
 
-#[Route(path: '/messenger-failed-queue/{id}/requeue', name: 'app.messenger_failed_queue_requeue', methods: ['PUT'])]
+#[Route(path: '/messenger-failed-queue/requeue', name: 'app.messenger_failed_queue_requeue', methods: ['PUT'])]
 final class RequeueController extends AbstractSecuredMessengerFailedQueueController implements SecuredControllerInterface
 {
     public function __construct(
@@ -19,10 +23,16 @@ final class RequeueController extends AbstractSecuredMessengerFailedQueueControl
     ) {
     }
 
-    public function __invoke(int $id, Request $request): Response
+    public function __invoke(Request $request): Response
     {
         try {
-            ($this->handler)($id);
+            $data = shape([
+                'identifiers' => vec(int()),
+            ])->coerce(json_decode($request->getContent(), true));
+
+            foreach ($data['identifiers'] as $id) {
+                ($this->handler)($id);
+            }
 
             return new JsonResponse(null, Response::HTTP_NO_CONTENT);
         } catch (\Throwable $e) {
