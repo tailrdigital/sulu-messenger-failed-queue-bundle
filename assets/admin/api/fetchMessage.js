6431ebcbc4a0efb7f5c9235e43d9aa9134cb388e
Prokyonn <daniel@sulu.io>
2024-03-01 00:47:09 +0100

    Add dynamic route generation

--- assets/admin/api/fetchMessage.js
+++ assets/admin/api/fetchMessage.js
@@ -1,6 +1,7 @@
 import {Requester} from "sulu-admin-bundle/services";
 import {runActionOnServer} from "../utilities/run-action-on-server";
 import {tryParseFailedMessage} from "../types/failed-message";
+import symfonyRouting from "fos-jsrouting/router";
 
 /**
  * @param {number} messageId
@@ -10,7 +11,7 @@ export async function fetchMessage(
     messageId,
 ) {
     const response = await runActionOnServer(
-        Requester.get(`/admin/api/messenger-failed-queue/${messageId}`)
+        Requester.get(symfonyRouting.generate('tailr.messenger_failed_queue_fetch', {id: messageId}))
     );
 
     return tryParseFailedMessage(response);
